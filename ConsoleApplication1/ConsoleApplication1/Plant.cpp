340a37616c185d00c1ae0f17ab94a12645f5f82a
Fusix10 <115464978+Fusix10@users.noreply.github.com>
2024-10-23 09:54:53 +0200

    Update, Plant && Bullet

--- ConsoleApplication1/ConsoleApplication1/Plant.cpp
+++ ConsoleApplication1/ConsoleApplication1/Plant.cpp
@@ -4,12 +4,14 @@ Plant::Plant()
 {
 }
 
-Plant::Plant(sf::Vector2f position, Behaviour* plant_behaviour, int ammo_count)
-{
-}
 
 Plant::~Plant()
 {
+}
+
+void Plant::Init(sf::Vector2f position, Behaviour* plant_behaviour, int ammo_count)
+{
+
 }
 
 int Plant::getAmmoCount() const
--- ConsoleApplication1/ConsoleApplication1/Plant.hpp
+++ ConsoleApplication1/ConsoleApplication1/Plant.hpp
@@ -8,12 +8,9 @@ class Plant : public Entity
     int mAmmoCount, mMaxAmmo;
 public:
     Plant();
-    Plant(
-        sf::Vector2f position,
-        Behaviour* plant_behaviour,
-        int ammo_count);
     ~Plant();
 
+    void Init(sf::Vector2f position, Behaviour* plant_behaviour, int ammo_count);
 
     void Update() override;
 
