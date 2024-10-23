b70c2609abc45edd21d7de8ad8ce97174d9589d2
Fusix10 <115464978+Fusix10@users.noreply.github.com>
2024-10-23 11:47:12 +0200

    [Update&&Add]: Bullet/Entity/ShootAction

--- ConsoleApplication1/ConsoleApplication1/Bullet.cpp
+++ ConsoleApplication1/ConsoleApplication1/Bullet.cpp
@@ -21,8 +21,3 @@ void Bullet::init(sf::Vector2f position, int row)
 void Bullet::Update()
 {
 }
-
-int Bullet::GetSpeed()
-{
-	return mSpeed;
-}
--- ConsoleApplication1/ConsoleApplication1/Bullet.h
+++ ConsoleApplication1/ConsoleApplication1/Bullet.h
@@ -7,6 +7,5 @@ public:
     ~Bullet();
     void init(sf::Vector2f position, int row);
     void Update() override;
-    int GetSpeed();
 };
 
